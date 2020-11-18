ce6ee11701f98573631aceebdb90b0d0457e2551
Hatemii <hatem.xhezairi96@gmail.com>
2020-11-18 19:59:23 +0100

    more details

--- frontend/src/components/Launch.js
+++ frontend/src/components/Launch.js
@@ -50,15 +50,41 @@ class Launch extends Component {
                                     <h1 className="display-4 my-3" >
                                         <span className="text-dark">Mission:</span>
                                         {mission_name}</h1>
+                                    <br />
+
+                                    <h3 className="mb-3" >Launch Details</h3>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">Flight Number: {flight_number}</li>
+                                        <li className="list-group-item">Launch Year: {launch_year}</li>
+                                        <li className="list-group-item">
+                                            Launch Success: <span className={classNames({
+                                            "text-success": launch_success,
+                                            "text-danger": !launch_success
+                                        })}>{launch_success ? "Yes" : "No"}</span>
+                                        </li>
+
+                                    </ul>
+                                    <br />
+                                    <br />
+
+                                    <h3 className="my-3">Rocket Details</h3>
+                                    <ul className="list-group">
+                                        <li className="list-group-item">Rocket ID: {rocket_id}</li>
+                                        <li className="list-group-item">Rocket Name: {rocket_name}</li>
+                                        <li className="list-group-item">Rocket Type: {rocket_type}</li>
+
+                                    </ul>
+
+                                    <hr />
+                                    <Link to="/" className="btn btn-secondary">Back</Link>
                                 </div>
                             );
-
                         }
                     }
 
                 </Query>
 
-            </Fragment>
+            </Fragment >
         );
     }
 }
