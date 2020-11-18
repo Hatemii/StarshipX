7bb7b25369ac4a76a3fcf28621cf74efd7f45e6f
Hatemii <hatem.xhezairi96@gmail.com>
2020-11-18 16:15:58 +0100

    Launches and LaunchItem

---
+++ frontend/src/components/LaunchItem.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default function LaunchItem({
+    launch: {
+        flight_number,
+        mission_name,
+        launch_date_local,
+        launch_success
+    } }) {
+
+    return (
+        <div>
+            <h2>Hi</h2>
+        </div>
+    )
+}
--- frontend/src/components/Launches.js
+++ frontend/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import gql from "graphql-tag"
 import { Query } from "react-apollo"
+import LaunchItem from "./LaunchItem"
 
 
 const LaunchesQuery = gql`
@@ -17,7 +18,7 @@ const LaunchesQuery = gql`
 export class Launches extends Component {
     render() {
         return (
-            <div>
+            <Fragment>
                 <h1 className="display-4 my-3" >Launches</h1>
                 <Query query={LaunchesQuery}>
                     {
@@ -25,14 +26,21 @@ export class Launches extends Component {
                             if (loading) return <h4>Loading ...</h4>
                             if (error) console.log(error);
 
-                            return <h1>test</h1>
+                            return (
+                                <Fragment>
+                                    {data.launches.map(launch => (
+                                        <LaunchItem key={launch.flight_number} launch={launch} />
+                                    ))}
+                                </Fragment>
+                            )
                         }
                     }
 
                 </Query>
-            </div>
+            </Fragment>
         )
     }
 }
+
 
 export default Launches
