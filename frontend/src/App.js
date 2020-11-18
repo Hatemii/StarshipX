e4b9225742b243a7329bf541f21c5132091bcbc3
Hatemii <hatem.xhezairi96@gmail.com>
2020-11-18 14:08:36 +0100

    changing project name to - frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react"
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+      <div className="App">
+        <h3>Space X</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
